6ea315bae04367103a78fb6d6c6904d439c9865c
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2024-01-11 16:15:02 +0300

    remove unused files

--- src/types/productionStepExecution.ts
+++
@@ -1,9 +0,0 @@
-export interface IPSEFormValues {
-  netWeight: number;
-}
-
-export interface IProductionStepExecution extends Partial<IPSEFormValues> {
-  endTime?: number;
-  status: "TODO" | "DONE";
-  name: string;
-}
--- src/utils/validations/productionStepExecutionSchema.ts
+++
@@ -1,5 +0,0 @@
-import * as Yup from "yup";
-
-export const PSEToDoneSchema = Yup.object().shape({
-  netWeight: Yup.number().required("Net weight required"),
-});
