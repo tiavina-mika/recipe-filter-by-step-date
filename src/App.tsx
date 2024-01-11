3e4f4147df03f11fece03a047a655eb68437b609
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2024-01-11 19:56:09 +0300

    add new data

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,35 @@ import utc from "dayjs/plugin/utc";
 dayjs.extend(utc);
 
 import PSEHeader from "./containers/PSEHeader";
-import { formatProductionItems } from "./utils/recipe.utils";
+import {
+  formatProductionItems,
+  getProductionStepsByStepDate
+} from "./utils/recipe.utils";
 import { productionItems } from "./utils/data/recipes";
 
+const endDate = 1706659200000;
+const startDate = 1703894400000;
+const prevStartDate = dayjs(startDate).add(-1, "days").startOf("day").valueOf();
+const nextEndDate = dayjs(endDate).add(1, "days").endOf("day").valueOf();
+
 const formattedRecipes = formatProductionItems(productionItems);
-console.log("formattedRecipes", formattedRecipes);
+// console.log("formattedRecipes", formattedRecipes);
+
+getProductionStepsByStepDate({
+  productionItems,
+  startDate,
+  endDate,
+  prevStartDate,
+  nextEndDate
+});
+
+// console.log("formattedRecipes", {
+//   startDate: dayjs(startDate).format("DD/MM/YYYY HH:mm"),
+//   endDate: dayjs(endDate).format("DD/MM/YYYY HH:mm"),
+//   prevStartDate: dayjs(prevStartDate).format("DD/MM/YYYY HH:mm"),
+//   nextEndDate: dayjs(nextEndDate).format("DD/MM/YYYY HH:mm"),
+// });
+
 const App = () => {
   return (
     <Box
--- src/utils/recipe.utils.ts
+++ src/utils/recipe.utils.ts
@@ -15,3 +15,75 @@ export const formatProductionItems = (productionItems) => {
     };
   });
 };
+
+/* @ts-ignore */
+
+const removeSteps = ({
+  productionSteps = [],
+  startDate,
+  endDate,
+  prevStartDate,
+  nextEndDate,
+  recipe
+}) => {
+  const filtteredSteps = [];
+  for (const productionStep of productionSteps) {
+    const step = productionStep.step;
+    const startDate1 = dayjs(prevStartDate)
+      .utc()
+      .add(+step.stepDate, "days")
+      .startOf("day")
+      .valueOf();
+    const endDate1 = dayjs(nextEndDate)
+      .utc()
+      .add(-step.stepDate, "days")
+      .endOf("day")
+      .valueOf();
+
+    if (
+      !step.productionSteps &&
+      recipe.name === "Création PSE - Recette B" &&
+      Math.round(dayjs(endDate1).diff(endDate, "day", true)) !== step.stepDate
+    ) {
+      console.log({
+        objectId: step.objectId,
+        recipe: recipe.name,
+        step: step.name,
+        dayDiff: step.stepDate,
+        stepComponent: step.stepComponents?.[0]?.supplierItem?.name,
+        stepComponent2:
+          step.stepComponents?.[0]?.priorSteps?.stepComponents?.[0]
+            ?.supplierItem?.name,
+        startDate: dayjs(startDate).format("DD/MM/YYYY HH:mm"),
+        endDate: dayjs(endDate).format("DD/MM/YYYY HH:mm"),
+        prevStartDate: dayjs(startDate1).format("DD/MM/YYYY HH:mm"),
+        nextEndDate: dayjs(endDate1).format("DD/MM/YYYY HH:mm"),
+        calcDiff: Math.round(dayjs(endDate1).diff(endDate, "day", true))
+      });
+    }
+  }
+
+  return filtteredSteps;
+};
+export const getProductionStepsByStepDate = ({
+  productionItems,
+  startDate,
+  endDate,
+  prevStartDate,
+  nextEndDate
+}) => {
+  for (const productionItem of productionItems) {
+    for (const section of productionItem.recipe.sections) {
+      const newSteps = removeSteps({
+        productionSteps: section.productionSteps,
+        startDate,
+        endDate,
+        prevStartDate,
+        nextEndDate,
+        recipe: productionItem.recipe
+      });
+    }
+  }
+
+  console.log("-------------");
+};
